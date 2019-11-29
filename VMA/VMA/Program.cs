981415dd5fe9643fcd07e73da262b4dc6d9c3c8b
suplakite <32765661+suplakite@users.noreply.github.com>
2019-11-29 19:21:54 +0100

    s
    s

--- VMA/VMA/Program.cs
+++ VMA/VMA/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace VMA
 {
@@ -17,6 +19,19 @@ namespace VMA
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_login());
+            SqlConnection con = new SqlConnection("Server=.\\MSSQLLocalDB;Database=Baza_projekt");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [dbo].[RentSet]", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{1}, {0}", reader.GetString(0), reader.GetString(1));
+
+            }
+            reader.Close();
+            con.Close();
+            Console.ReadLine();
+
         }
     }
 }
