bdf4dd9614afb15b275f458cc6cc4e9ab7ce5822
suplakite <32765661+suplakite@users.noreply.github.com>
2019-11-30 10:45:37 +0100

    Kierownik czy nie

--- VMA/VMA/Form1.cs
+++ VMA/VMA/Form1.cs
@@ -35,12 +35,30 @@ namespace VMA
             try
             {
                 var user = db.WorkerSets.Where(i => i.password == password && i.name == name).Single();
+
                 if (user != null)
                 {
+                    var position = (from WorkerSet W in db.WorkerSets
+                                    where W.name == name
+                                    select W.position).Single();
+                    if (position == "kierownik")
+                    {
+                        textBox_login_name.Clear();
+                        textBox_login_password.Clear();
+                        adminapp = new Form_panel_admin(this);
+                        adminapp.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                   
+                        textBox_login_name.Clear();
+                        textBox_login_password.Clear();
+                        mainapp = new MainApp(this, name, "Pietryja");
+                        mainapp.Show();
+                        this.Hide();
+                    }
                     
-                    mainapp = new MainApp(this, name, "Pietryja");
-                    mainapp.Show();
-                    this.Hide();
                 }
             }
             catch(Exception)
