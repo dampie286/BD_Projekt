4ec9bc44ce4a20af9a3bc6cbd06882b40d077457
Piotr Kwiatkowski <zaszas61@hotmail.com>
2019-12-01 17:59:26 +0100

    Update UserControl_reservation.Designer.cs

--- VMA/VMA/UserControl_reservation.Designer.cs
+++ VMA/VMA/UserControl_reservation.Designer.cs
@@ -63,7 +63,7 @@
             this.button_filter.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button_filter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
             this.button_filter.Location = new System.Drawing.Point(610, 15);
-            this.button_filter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_filter.Margin = new System.Windows.Forms.Padding(2);
             this.button_filter.Name = "button_filter";
             this.button_filter.Size = new System.Drawing.Size(75, 25);
             this.button_filter.TabIndex = 13;
@@ -77,7 +77,7 @@
             this.textBox_license.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_license.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_license.Location = new System.Drawing.Point(493, 19);
-            this.textBox_license.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_license.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_license.MaximumSize = new System.Drawing.Size(100, 25);
             this.textBox_license.MinimumSize = new System.Drawing.Size(4, 15);
             this.textBox_license.Name = "textBox_license";
@@ -93,7 +93,7 @@
             this.textBox_mileage.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_mileage.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_mileage.Location = new System.Drawing.Point(398, 19);
-            this.textBox_mileage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_mileage.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_mileage.MaximumSize = new System.Drawing.Size(100, 25);
             this.textBox_mileage.MinimumSize = new System.Drawing.Size(4, 15);
             this.textBox_mileage.Name = "textBox_mileage";
@@ -109,7 +109,7 @@
             this.textBox_version.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_version.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_version.Location = new System.Drawing.Point(303, 19);
-            this.textBox_version.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_version.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_version.MaximumSize = new System.Drawing.Size(100, 25);
             this.textBox_version.MinimumSize = new System.Drawing.Size(4, 15);
             this.textBox_version.Name = "textBox_version";
@@ -125,7 +125,7 @@
             this.textBox_equipment.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_equipment.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_equipment.Location = new System.Drawing.Point(208, 19);
-            this.textBox_equipment.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_equipment.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_equipment.MaximumSize = new System.Drawing.Size(100, 25);
             this.textBox_equipment.MinimumSize = new System.Drawing.Size(4, 15);
             this.textBox_equipment.Name = "textBox_equipment";
@@ -141,7 +141,7 @@
             this.textBox_model.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_model.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_model.Location = new System.Drawing.Point(113, 19);
-            this.textBox_model.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_model.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_model.MaximumSize = new System.Drawing.Size(100, 25);
             this.textBox_model.MinimumSize = new System.Drawing.Size(4, 15);
             this.textBox_model.Name = "textBox_model";
@@ -157,7 +157,7 @@
             this.textBox_brand.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_brand.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_brand.Location = new System.Drawing.Point(18, 19);
-            this.textBox_brand.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_brand.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_brand.MaximumSize = new System.Drawing.Size(100, 25);
             this.textBox_brand.MinimumSize = new System.Drawing.Size(4, 15);
             this.textBox_brand.Name = "textBox_brand";
@@ -256,14 +256,16 @@
             this.textBox_reser_license.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_reser_license.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_reser_license.Location = new System.Drawing.Point(68, 281);
-            this.textBox_reser_license.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_reser_license.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_reser_license.MaximumSize = new System.Drawing.Size(150, 25);
             this.textBox_reser_license.MinimumSize = new System.Drawing.Size(4, 18);
             this.textBox_reser_license.Name = "textBox_reser_license";
-            this.textBox_reser_license.Size = new System.Drawing.Size(130, 18);
+            this.textBox_reser_license.Size = new System.Drawing.Size(130, 15);
             this.textBox_reser_license.TabIndex = 22;
             this.textBox_reser_license.Text = "Nr rejestracyjny";
             this.textBox_reser_license.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox_reser_license.Enter += new System.EventHandler(this.textBox_reser_license_Enter);
+            this.textBox_reser_license.Leave += new System.EventHandler(this.textBox_reser_license_Leave);
             // 
             // panel8
             // 
@@ -280,23 +282,27 @@
             this.textBox_reserv_purpose.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox_reserv_purpose.ForeColor = System.Drawing.Color.Yellow;
             this.textBox_reserv_purpose.Location = new System.Drawing.Point(68, 359);
-            this.textBox_reserv_purpose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_reserv_purpose.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_reserv_purpose.MaximumSize = new System.Drawing.Size(150, 25);
             this.textBox_reserv_purpose.MinimumSize = new System.Drawing.Size(4, 18);
             this.textBox_reserv_purpose.Name = "textBox_reserv_purpose";
-            this.textBox_reserv_purpose.Size = new System.Drawing.Size(130, 18);
+            this.textBox_reserv_purpose.Size = new System.Drawing.Size(130, 15);
             this.textBox_reserv_purpose.TabIndex = 24;
             this.textBox_reserv_purpose.Text = "Cel rezerwacji";
             this.textBox_reserv_purpose.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox_reserv_purpose.Enter += new System.EventHandler(this.textBox_reserv_purpose_Enter);
+            this.textBox_reserv_purpose.Leave += new System.EventHandler(this.textBox_reserv_purpose_Leave);
             // 
             // dateTimePicker_from_date_reserv
             // 
             this.dateTimePicker_from_date_reserv.CalendarFont = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.dateTimePicker_from_date_reserv.CalendarForeColor = System.Drawing.Color.Yellow;
             this.dateTimePicker_from_date_reserv.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.dateTimePicker_from_date_reserv.CustomFormat = "yyyy-MM-dd";
             this.dateTimePicker_from_date_reserv.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker_from_date_reserv.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker_from_date_reserv.Location = new System.Drawing.Point(425, 279);
-            this.dateTimePicker_from_date_reserv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker_from_date_reserv.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker_from_date_reserv.Name = "dateTimePicker_from_date_reserv";
             this.dateTimePicker_from_date_reserv.Size = new System.Drawing.Size(151, 20);
             this.dateTimePicker_from_date_reserv.TabIndex = 26;
@@ -331,9 +337,11 @@
             this.dateTimePicker_to_date_reserv.CalendarFont = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.dateTimePicker_to_date_reserv.CalendarForeColor = System.Drawing.Color.Yellow;
             this.dateTimePicker_to_date_reserv.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.dateTimePicker_to_date_reserv.CustomFormat = "yyyy-MM-dd";
             this.dateTimePicker_to_date_reserv.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker_to_date_reserv.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker_to_date_reserv.Location = new System.Drawing.Point(425, 357);
-            this.dateTimePicker_to_date_reserv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker_to_date_reserv.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker_to_date_reserv.Name = "dateTimePicker_to_date_reserv";
             this.dateTimePicker_to_date_reserv.Size = new System.Drawing.Size(151, 20);
             this.dateTimePicker_to_date_reserv.TabIndex = 29;
@@ -346,12 +354,13 @@
             this.button_reserv.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button_reserv.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
             this.button_reserv.Location = new System.Drawing.Point(259, 433);
-            this.button_reserv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_reserv.Margin = new System.Windows.Forms.Padding(2);
             this.button_reserv.Name = "button_reserv";
             this.button_reserv.Size = new System.Drawing.Size(178, 34);
             this.button_reserv.TabIndex = 31;
             this.button_reserv.Text = "Rezerwuj";
             this.button_reserv.UseVisualStyleBackColor = false;
+            this.button_reserv.Click += new System.EventHandler(this.button_reserv_Click);
             // 
             // UserControl_reservation
             // 
