e72129ae9f8f671d351b2dc7df57668840df42f6
Piotr Kwiatkowski <zaszas61@hotmail.com>
2019-12-01 17:59:31 +0100

    Update UserControl_reservation.cs

--- VMA/VMA/UserControl_reservation.cs
+++ VMA/VMA/UserControl_reservation.cs
@@ -12,6 +12,7 @@ namespace VMA
 {
     public partial class UserControl_reservation : UserControl
     {
+        int workera_id;
         public UserControl_reservation()
         {
             InitializeComponent();
@@ -19,11 +20,14 @@ namespace VMA
 
         public void fillDataGridView(DataTable tmp)     //uzupełnienie tabeli
         {
-            dataGridView_veh_DB.DataSource = tmp;
+            DataBaseDataContext db = new DataBaseDataContext();
+            var Selectquery = from x in db.VehicleSets join y in db.ReservationSets on x.vehicle_id equals y.Vehicle_vehicle_id join z in db.WorkerSets on y.Worker_worker_id equals z.worker_id  select new { REJESTRACJA = x.licence_plate, MARKA = x.brand, MODEL = x.model,OD = y.date_from, DO = y.date_to, REZERWUJACY = z.surname};
+
+            dataGridView_veh_DB.DataSource = Selectquery;
 
 
 
-            dataGridView_veh_DB.Columns[0].Visible = false;
+            //dataGridView_veh_DB.Columns[0].Visible = false;
             dataGridView_veh_DB.RowHeadersVisible = false;
             dataGridView_veh_DB.ReadOnly = true;        //nie moze edytować kolumn
             
@@ -32,9 +36,9 @@ namespace VMA
             dataGridView_veh_DB.Columns[1].Width = 60;
             dataGridView_veh_DB.Columns[2].Width = 60;
             dataGridView_veh_DB.Columns[3].Width = 60;
-            dataGridView_veh_DB.Columns[4].Width = 90;
-            dataGridView_veh_DB.Columns[5].Width = 90;
-            dataGridView_veh_DB.Columns[6].Width = 90;
+            //dataGridView_veh_DB.Columns[4].Width = 90;
+            //dataGridView_veh_DB.Columns[5].Width = 90;
+            //dataGridView_veh_DB.Columns[6].Width = 90;
 
         }
 
@@ -49,7 +53,61 @@ namespace VMA
             dateTimePicker_from_date_reserv.MinDate = DateTime.Today;
 
         }
+        public void setWorkerID(int worker_idB)
+        {
+           workera_id = worker_idB;
+        }
 
-       
+        private void button_reserv_Click(object sender, EventArgs e)
+        {
+            using (DataBaseDataContext db = new DataBaseDataContext())
+            {
+                var vechicle_id = db.VehicleSets.Where(i => i.licence_plate == Convert.ToString(textBox_reser_license.Text)).Single();
+                var worker_id = db.WorkerSets.Where(j => j.worker_id == workera_id).Single();
+                ReservationSet reserv = new ReservationSet()
+                {
+                    purpose = Convert.ToString(textBox_reserv_purpose.Text),
+                    date_from = dateTimePicker_from_date_reserv.Value,
+                    date_to = dateTimePicker_to_date_reserv.Value,
+                    Worker_worker_id = worker_id.worker_id,
+                    Vehicle_vehicle_id = vechicle_id.vehicle_id
+                };
+
+                db.ReservationSets.InsertOnSubmit(reserv);
+                db.SubmitChanges();
+            }
+        }
+
+        private void textBox_reser_license_Enter(object sender, EventArgs e)
+        {
+            if (textBox_reser_license.Text.Equals(@"Nr rejestracyjny"))
+            {
+                textBox_reser_license.Text = "";
+            }
+        }
+
+        private void textBox_reser_license_Leave(object sender, EventArgs e)
+        {
+            if (textBox_reser_license.Text.Equals(""))
+            {
+                textBox_reser_license.Text = "Nr rejestracyjny";
+            }
+        }
+
+        private void textBox_reserv_purpose_Leave(object sender, EventArgs e)
+        {
+            if (textBox_reserv_purpose.Text.Equals(""))
+            {
+                textBox_reserv_purpose.Text = "Cel rezerwacji";
+            }
+        }
+
+        private void textBox_reserv_purpose_Enter(object sender, EventArgs e)
+        {
+            if (textBox_reserv_purpose.Text.Equals(@"Cel rezerwacji"))
+            {
+                textBox_reserv_purpose.Text = "";
+            }
+        }
     }
 }
