5c817680ed0a5bb567b9a38305bc4f4f2fed0c5c
Piotr Kwiatkowski <zaszas61@hotmail.com>
2019-12-05 13:33:54 +0100

    usuwanie opieki

--- VMA/VMA/UserControl_delete_keeper.cs
+++ VMA/VMA/UserControl_delete_keeper.cs
@@ -18,43 +18,7 @@ namespace VMA
             InitializeComponent();
         }
 
-        private void Grid_edit()
-        {
-
-            //widocznos kolumn
-            //  dataGridView_workers_DB.RowHeadersVisible = false;
-            //dataGridView_keepers_DB.Columns[0].Visible = false;
-            //  dataGridView_workers_DB.Columns[1].Visible = false;
-            // dataGridView_workers_DB.Columns[2].Visible = false;
-            //dataGridView_workers_DB.Columns[3].Visible = false;
-            //dataGridView_workers_DB.Columns[4].Visible = false;
-            //dataGridView_keepers_DB.Columns[5].Visible = false;
-            //dataGridView_keepers_DB.Columns[6].Visible = false;
-            // dataGridView_workers_DB.Columns[7].Visible = false;
-            //dataGridView_keepers_DB.Columns[8].Visible = false;
-            //opcje kolumn w edycji
-
-            //dataGridView_keepers_DB.Columns[0].ReadOnly = true;//id
-            //dataGridView_keepers_DB.Columns[1].ReadOnly = true;//imie
-            //dataGridView_keepers_DB.Columns[2].ReadOnly = false;//nazwisko
-            //dataGridView_keepers_DB.Columns[3].ReadOnly = false;//pozycja
-           // dataGridView_keepers_DB.Columns[4].ReadOnly = true;//pesel
-            //dataGridView_keepers_DB.Columns[5].ReadOnly = true;//data urodzenia
-            //dataGridView_keepers_DB.Columns[6].ReadOnly = false;//haslo
-            //dataGridView_keepers_DB.Columns[7].ReadOnly = false;//telefon
-            //dataGridView_keepers_DB.Columns[8].Visible = false;//keeper
-
-
-
-            //rozmiary kolumn
-            
-            //dataGridView_keepers_DB.Columns[1].Width = 100;
-            //dataGridView_keepers_DB.Columns[2].Width = 150;
-            //dataGridView_keepers_DB.Columns[3].Width = 100;
-            //dataGridView_keepers_DB.Columns[4].Width = 100;
-            //dataGridView_keepers_DB.Columns[7].Width = 100;
-
-        }
+     
 
         public void fillDataGridView2()
         {
@@ -64,7 +28,7 @@ namespace VMA
          
           var query = from x in db.CareSets select new { IMIE = x.WorkerSet_Keeper.WorkerSet.name, NAZWISKO = x.WorkerSet_Keeper.WorkerSet.surname, OPIEKA = x.VehicleSet.model, REJESTRACJA = x.VehicleSet.licence_plate };
             dataGridView_keepers_DB.DataSource = query;
-            Grid_edit();
+           
 
         }
 
@@ -119,6 +83,10 @@ namespace VMA
         private void button_delete_Click(object sender, EventArgs e)
         {
 
+
+
+            
+            System.Diagnostics.Process.Start("https://youtu.be/XqZsoesa55w?t=29");
         }
     }
 }
