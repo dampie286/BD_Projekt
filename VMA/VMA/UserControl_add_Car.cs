ec61f58726c149e413d90aa3b5105e9385f3df12
Piotr Kwiatkowski <zaszas61@hotmail.com>
2019-12-05 17:27:07 +0100

    komunikat o dodaniu auta

--- VMA/VMA/UserControl_add_Car.cs
+++ VMA/VMA/UserControl_add_Car.cs
@@ -20,6 +20,7 @@ namespace VMA
         private void button_add_car_Click(object sender, EventArgs e)
         {
             double consump;
+            bool confirm=false;
             if (textBox_avg_fuel.Text.Contains("."))
             {
                 consump = Convert.ToDouble(textBox_avg_fuel.Text.Replace('.', ','));
@@ -31,19 +32,31 @@ namespace VMA
 
             using (DataBaseDataContext db = new DataBaseDataContext())
             {
-                VehicleSet car = new VehicleSet()
+                try
                 {
-                    brand = Convert.ToString(textBox_brand.Text),
-                    model = Convert.ToString(textBox_model.Text),
-                    version = Convert.ToString(comboBox_car_version.Text),
-                    equipment="",
-                    licence_plate = Convert.ToString(textBox_license.Text),
-                    fuel_type = Convert.ToString(comboBox_type_of_fuel.Text),
-                    avg_consumption = Convert.ToDouble(consump),
-                    available = "yes"
-                };
-                db.VehicleSets.InsertOnSubmit(car);
-                db.SubmitChanges();
+                    VehicleSet car = new VehicleSet()
+                    {
+                        brand = Convert.ToString(textBox_brand.Text),
+                        model = Convert.ToString(textBox_model.Text),
+                        version = Convert.ToString(comboBox_car_version.Text),
+                        equipment = "",
+                        licence_plate = Convert.ToString(textBox_license.Text),
+                        fuel_type = Convert.ToString(comboBox_type_of_fuel.Text),
+                        avg_consumption = Convert.ToDouble(consump),
+                        available = "yes"
+                    };
+                    db.VehicleSets.InsertOnSubmit(car);
+                    db.SubmitChanges();
+                    confirm = true;
+                }
+                catch
+                {
+
+                    MessageBox.Show("Bład dodania auta", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                if (confirm)
+                    MessageBox.Show("Dodano auto", "Potwierdzenie", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
         }
        
