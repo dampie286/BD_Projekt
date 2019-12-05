aef2f940eb890e7cd8f6934eeaf7752b4c1c06b0
dampie286 <damian.pietryja@gmail.com>
2019-12-05 17:21:17 +0100

    Zmienione modyfikacja samochodu

--- VMA/VMA/UserControl_add_Car.cs
+++ VMA/VMA/UserControl_add_Car.cs
@@ -35,10 +35,10 @@ namespace VMA
                 {
                     brand = Convert.ToString(textBox_brand.Text),
                     model = Convert.ToString(textBox_model.Text),
-                    version = Convert.ToString(textBox_fuel_type.Text),
-                    equipment = Convert.ToString(textBox_version.Text),
+                    //version = Convert.ToString(textBox_fuel_type.Text),
+                    //equipment = Convert.ToString(textBox_version.Text),
                     licence_plate = Convert.ToString(textBox_license.Text),
-                    fuel_type = Convert.ToString(textBox_fuel_type.Text),
+                   // fuel_type = Convert.ToString(textBox_fuel_type.Text),
                     avg_consumption = Convert.ToDouble(consump),
                     available = "yes"
                 };
@@ -63,15 +63,9 @@ namespace VMA
             textBox_age.Clear();
         }
 
-        private void textBox_version_Click(object sender, EventArgs e)
-        {
-            textBox_version.Clear();
-        }
+      
 
-        private void textBox_fuel_type_Click(object sender, EventArgs e)
-        {
-            textBox_fuel_type.Clear();
-        }
+       
 
         private void textBox_license_Click(object sender, EventArgs e)
         {
@@ -88,10 +82,7 @@ namespace VMA
             textBox_avg_fuel.Clear();
         }
 
-        private void UserControl_add_Car_Load(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
 
